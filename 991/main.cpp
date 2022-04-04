355ee0d672a3265b680b3596d8429888ea1583b5
He Jidong <1941109171@qq.com>
2022-04-04 20:30:11 +0800

    Initial Commit

---
+++ 991/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  991
+//
+//  Created by 何纪栋 on 2022/4/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

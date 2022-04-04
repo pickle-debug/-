ef68415167944b1a4a73524a07497034f8eea779
He Jidong <1941109171@qq.com>
2022-04-04 20:34:19 +0800

    first commit

---
+++ 991/DataStruct/P18.cpp
@@ -0,0 +1,8 @@
+//
+//  P18.cpp
+//  991
+//
+//  Created by 何纪栋 on 2022/4/4.
+//
+
+#include "P18.hpp"
---
+++ 991/DataStruct/P18.hpp
@@ -0,0 +1,13 @@
+//
+//  P18.hpp
+//  991
+//
+//  Created by 何纪栋 on 2022/4/4.
+//
+
+#ifndef P18_hpp
+#define P18_hpp
+
+#include <stdio.h>
+
+#endif /* P18_hpp */
